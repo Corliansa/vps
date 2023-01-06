fcc2ebbfa33d96fd9e6db2f0e56b8105427478b1
Corliansa Kusumah <corliansa@gmail.com>
2023-01-06 16:09:04 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,25 @@
+# Install docker engine
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Create folder
+mkdir -p ./nginx/conf
+mkdir -p ./certbot/conf
+mkdir -p ./certbot/www
+mkdir -p ./minio/data
+sudo chown 1001 ./minio/data
+
+# Download tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up
