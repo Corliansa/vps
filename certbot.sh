4575bb6e9a64ac1015f89d180dbe11f8a9d54a13
Corliansa Kusumah <corliansa@gmail.com>
2023-01-06 20:42:14 +0100

    fixed pre conf and cert bot

--- certbot.sh
+++ certbot.sh
@@ -1,1 +1,1 @@
-docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d corliansa.xyz
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d corliansa.xyz
