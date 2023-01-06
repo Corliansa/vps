38a0036002f94203242facfedb6f017f80fcb0a1
Corliansa Kusumah <corliansa@gmail.com>
2023-01-06 21:14:16 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -23,3 +23,6 @@ sudo chown 1001 ./minio/data
 # Setup tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --advertise-exit-node
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
