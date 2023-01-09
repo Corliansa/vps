28deccbbbce04f9511daa3f72a830b3c9b8471b5
Corliansa Kusumah <corliansa@gmail.com>
2023-01-09 15:57:22 +0100

    helper scripts

---
+++ replacer.sh
@@ -0,0 +1,8 @@
+DOMAIN=example.com
+
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" docker-compose.yml
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" certbot.sh 
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/default.conf
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/conf/default.conf
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/app.conf 
+find . -name "*.bak" -type f -delete
--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 # Setup docker
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# only if docker is not installed
+if [ ! -x "$(command -v docker)" ]; then
+  sudo apt-get update
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+fi
 
 # Setup folders
 mkdir -p ./nginx/conf
@@ -31,6 +34,6 @@ systemctl reload-or-restart systemd-resolved
 # Setup tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --advertise-exit-node
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+grep 'net.ipv4.ip_forward = 1' /etc/sysctl.d/99-tailscale.conf && echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+grep 'net.ipv6.conf.all.forwarding = 1' /etc/sysctl.d/99-tailscale.conf && echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
