d03f0b6069af18d9758e2cd2452bdb905cd9be07
Corliansa Kusumah <corliansa@gmail.com>
2023-01-11 15:43:28 +0100

    purged unused services

--- certbot.sh
+++ certbot.sh
@@ -1,5 +1,4 @@
 docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d corliansa.xyz
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d s3.corliansa.xyz
 docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d dns.corliansa.xyz
 find . -name 'privkey.pem' -execdir cp {} 'private.key' ';'
 find . -name 'fullchain.pem' -execdir cp {} 'public.crt' ';'
