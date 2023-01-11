3de6a349be71a3f55c56424b7c4ba98ec165ff63
Corliansa Kusumah <corliansa@gmail.com>
2023-01-11 11:53:29 +0100

    successfully integrated coolify traefik with nginx

--- conf.sh
+++ conf.sh
@@ -1,3 +1,3 @@
-mv -f nginx/default.conf nginx/conf/default.conf
+mv -f nginx/default.stream.conf nginx/conf/default.stream.conf
 mv -f nginx/app.conf nginx/conf/app.conf
 sed -z 's/# uncomment after ssl certificate is generated\n\s*#\s*//g' -i docker-compose.yml
--- replacer.sh
+++ replacer.sh
@@ -2,7 +2,7 @@ DOMAIN=example.com
 
 sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" docker-compose.yml
 sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" certbot.sh 
-sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/default.conf
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/default.stream.conf
 sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/conf/default.conf
 sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" nginx/app.conf 
 find . -name "*.bak" -type f -delete
