ac43680146a8158d7775658db428ae3effdfaaec
Corliansa Kusumah <corliansa@gmail.com>
2023-01-19 07:49:37 +0100

    added certs dumper

--- certbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-traefik-certs-dumper file --version v2 --source ./letsencrypt/acme/acme.json --domain-subdir=true --dest ./letsencrypt/certs --watch
--- replacer.sh
+++ replacer.sh
@@ -7,6 +7,6 @@ fi
 DOMAIN=$1
 
 sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" docker-compose.yml
-sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" certbot.sh
+sed -i.bak "s/corliansa.xyz/${DOMAIN}/g" replacer/data/data.js
 
 find . -name "*.bak" -type f -delete
