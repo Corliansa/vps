4a8b42efbb8c4542e22d1c1ac9871fe9125af6ae
Corliansa Kusumah <corliansa@gmail.com>
2023-01-19 15:39:43 +0100

    protected coolify and portainer

--- replacer/data/data.js
+++ replacer/data/data.js
@@ -35,6 +35,26 @@ module.exports = {
         },
         middlewares: [],
       },
+      "portainer-router": {
+        entrypoints: ["web"],
+        rule: "Host(`portainer.corliansa.xyz`) && PathPrefix(`/`)",
+        service: "noop",
+        priority: 2,
+        middlewares: ["redirect-to-https"],
+      },
+      "portainer-router-secure": {
+        entrypoints: ["websecure"],
+        rule: "Host(`portainer.corliansa.xyz`) && PathPrefix(`/`)",
+        service: "portainer",
+        priority: 2,
+        tls: {
+          certresolver: "letsencrypt",
+        },
+        middlewares: ["tailscale-auth"],
+      },
+      "coolify-3000-/-secure": {
+        middlewares: ["tailscale-auth"],
+      },
     },
     services: {
       nextjs: {
@@ -51,6 +71,16 @@ module.exports = {
           servers: [
             {
               url: "https://adguard:443",
+              scheme: "https",
+            },
+          ],
+        },
+      },
+      portainer: {
+        loadbalancer: {
+          servers: [
+            {
+              url: "https://portainer:9443",
               scheme: "https",
             },
           ],
